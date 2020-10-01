73956dd7fb90381a7ab553dfd5a23a32e5ca2fba
Edvard Bakken <ed@edbook.local>
2020-10-01 07:18:02 +0200

    Forn placeholder text update

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <div class="flex h-screen">
       <div class="m-auto py-16 px-8 md:px-16 bg-white w-11/12 max-w-md">
-        <div class="text-4xl font-bold text-center">URL Shortener</div>
-        <input v-model="key" v-on:keydown="status='writing'" class="mt-8 bg-white border border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" placeholder="/KEY">
-        <input v-model="url" v-on:keydown="status='writing'" class="mt-8 bg-white border border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="url" placeholder="URL">
+        <div class="text-4xl font-bold text-center">Edvard.link</div>
+        <input v-model="key" v-on:keydown="status='writing'" class="mt-8 bg-white border border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" placeholder="edvard.link/KEY">
+        <input v-model="url" v-on:keydown="status='writing'" class="mt-8 bg-white border border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="url" placeholder="https://example.com">
         <div v-if="status=='existing'" class="mt-8 bg-red-200 py-2 px-4">Key <span class="font-bold italic">{{key}}</span> already in use</div>
         <div v-if="status=='missing'" class="mt-8 bg-yellow-200 py-2 px-4">Missing values</div>
         <div v-if="status=='created'" class="mt-8 bg-green-200 py-2 px-4">
